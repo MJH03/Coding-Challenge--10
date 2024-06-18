9b23864ffa6123e812e5ad588b2d303ca490828f
MJH03 <mjhandelsman@usf.edu>
2024-06-18 16:49:25 -0400

    Add sample CSV data file

--- example.js
+++ example.js
@@ -1,1 +1,19 @@
 //U6899-7808
+//Step 2: JavaScript Implementation
+function main() {
+    d3.csv('../purchase_orders.csv', d3.autoType).then(
+        function (d) {
+            for (let index = 0; index < d.length; index++) {
+                const element = d[index];
+                const unorderedList = d3.select('body').selectAll('ul')
+                unorderedList.selectAll('li')
+                    .data(d)
+                    .enter()
+                    .append("li")
+                    .text(function (d){
+                                return `${d.customerName} - ${d.orderId}: ${d.purchaseAmount}`
+                    })
+            }
+        }
+    )
+}
