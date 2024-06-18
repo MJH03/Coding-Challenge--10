49eb8e83c1441aba817eac56a540de6d83a51069
MJH03 <mjhandelsman@usf.edu>
2024-06-18 16:51:27 -0400

    Finalize Data Display Functionality

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 //U6899-7808
 //Step 2: JavaScript Implementation
-function main() {
-    d3.csv('../purchase_orders.csv', d3.autoType).then(
+function main() {                     // Step 4 Data loading and display
+    d3.dsv('../purchase_orders.csv', d3.autoType).then(
         function (d) {
             for (let index = 0; index < d.length; index++) {
                 const element = d[index];
